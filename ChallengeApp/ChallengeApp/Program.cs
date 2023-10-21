d3ad1fc704711ab8ae432086e83b6f15f34d531a
DariuszSliwa <dariuszsliwa0@wp.pl>
2023-10-21 21:35:16 +0200

    Zadanie, dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var gender = "woman";
+var age = 33;
+
+if (gender == "woman" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+else if (gender == "man" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
+else
+{
+    Console.WriteLine("Nieprawidłowe dane");
+}
