85f9e2360c5f446358c80ebbe59ac8d4c6c68b0f
DariuszSliwa <dariuszsliwa0@wp.pl>
2023-11-01 20:21:41 +0100

    Zadanie, dzień 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,36 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+
+        public Employee(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+
+        public string showInformationAboutEmployee
+        {
+            get 
+            {
+                return "Pracownik/ca z największą liczbą punktów: " + this.Name + " " + this.Surname + ", uzyskał/a punktów: " + Result;
+            }
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,22 +1,36 @@
-﻿int number = 4566;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToArray();
+﻿using ChallengeApp;
 
-int[] results = new int[10];
-foreach (var letter in letters)
+Employee employee1 = new Employee("Adam", "Nowicki");
+Employee employee2 = new Employee("Dariusz", "Malinowski");
+Employee employee3 = new Employee("Aleksandra", "Makowiecka");
+
+employee1.AddScore(5);
+employee1.AddScore(10);
+employee1.AddScore(8);
+
+employee2.AddScore(9);
+employee2.AddScore(8);
+employee2.AddScore(7);
+
+employee3.AddScore(1);
+employee3.AddScore(4);
+employee3.AddScore(10);
+
+List<Employee> employees = new List<Employee>()
+{ 
+    employee1, employee2, employee3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach(var employee in employees)
 {
-    for (int i = 0; i < results.Length; i++)
+    if(employee.Result > maxResult)
     {
-        int a = letter - '0';
-        if (a == i)
-        {
-            results[i]++;
-        }
+        maxResult = employee.Result;
+        employeeWithMaxResult = employee;
     }
 }
 
-Console.WriteLine("Wyniki dla liczby: " + number);
-for (int i = 0; i < results.Length; i++)
-{
-    Console.WriteLine(i + " => " + results[i]);
-}
+Console.WriteLine(employeeWithMaxResult.showInformationAboutEmployee);
