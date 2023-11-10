f535a54e80ff3c99d16f5c2856c2488570d0134c
DariuszSliwa <dariuszsliwa0@wp.pl>
2023-11-10 21:04:13 +0100

    Zadanie, dzień 8

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,49 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void ComperingIntType()
+        {
+            // arrange
+            int number1 = 2;
+            int number2 = 2;
+
+            // act
+
+            //action
+            Assert.AreEqual(number1, number2);
+        }
+
+        [Test]
+        public void ComperingStringType()
+        {
+            // arrange
+            string name1 = "Darek";
+            string name2 = "Darek";
+
+            // act
+
+            //action
+            Assert.AreEqual(name1, name2);
+        }
+
+        [Test]
+        public void ComperingReferenceType()
+        {
+            // arrange
+            var employee1 = GetEmployee("Andrzej", "Kowalski", 49);
+            var employee2 = GetEmployee("Michał", "Sapalski", 32);
+
+            // act
+
+            //action
+            Assert.AreNotEqual(employee1, employee2);
+        }
+
+        private Employee GetEmployee(string name, string surname, int age)
+        {
+            return new Employee(name, surname, age);
+        }
+    }
+}
