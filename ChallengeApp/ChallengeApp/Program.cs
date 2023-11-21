72b451928888241b06e1b80bd0362425deeef173
DariuszSliwa <dariuszsliwa0@wp.pl>
2023-11-21 19:12:56 +0100

    Zadanie, dzień 11, poprawione

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -14,5 +14,17 @@ var statistics3 = employee1.GetStatisticsDoWhile();
 var statistics4 = employee1.GetStatisticsWhile();
 
 Console.WriteLine($"Average: {statistics1.Average:N2}");
+Console.WriteLine($"Min: {statistics1.Min}");
+Console.WriteLine($"Max: {statistics1.Max}");
+
+Console.WriteLine($"Average: {statistics2.Average:N2}");
 Console.WriteLine($"Min: {statistics2.Min}");
+Console.WriteLine($"Max: {statistics2.Max}");
+
+Console.WriteLine($"Average: {statistics3.Average:N2}");
+Console.WriteLine($"Min: {statistics3.Min}");
 Console.WriteLine($"Max: {statistics3.Max}");
+
+Console.WriteLine($"Average: {statistics4.Average:N2}");
+Console.WriteLine($"Min: {statistics4.Min}");
+Console.WriteLine($"Max: {statistics4.Max}");
