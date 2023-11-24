a6d258f8c4efb309091123099fa8f92435b4da5b
DariuszSliwa <dariuszsliwa0@wp.pl>
2023-11-24 17:43:01 +0100

    Zadanie, dzień 15, poprawiona wersja

--- ChallengeApp/ChallengeApp/Supervisor.cs
+++ ChallengeApp/ChallengeApp/Supervisor.cs
@@ -18,7 +18,14 @@
 
         public void AddGrade(float grade)
         {
-            throw new NotImplementedException();
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                throw new Exception("invalid grade value");
+            }
         }
 
         public void AddGrade(string grade)
@@ -83,32 +90,102 @@
                 case "1":
                     this.grades.Add(0);
                     break;
+                default:
+                    throw new Exception("invalid string");
             }
         }
 
         public void AddGrade(long grade)
         {
-            throw new NotImplementedException();
+            float result = grade;
+            this.AddGrade(result);
         }
 
         public void AddGrade(int grade)
         {
-            throw new NotImplementedException();
+            float result = grade;
+            this.AddGrade(result);
         }
 
         public void AddGrade(double grade)
         {
-            throw new NotImplementedException();
+            float result = (float)grade;
+            this.AddGrade(result);
         }
 
         public void AddGrade(char grade)
         {
-            throw new NotImplementedException();
+            if (char.IsDigit(grade))
+            {
+                float result = (float)(grade - '0');
+                this.AddGrade(result);
+            }
+            else
+            {
+                switch (grade)
+                {
+                    case 'A':
+                    case 'a':
+                        this.grades.Add(100);
+                        break;
+                    case 'B':
+                    case 'b':
+                        this.grades.Add(80);
+                        break;
+                    case 'C':
+                    case 'c':
+                        this.grades.Add(60);
+                        break;
+                    case 'D':
+                    case 'd':
+                        this.grades.Add(40);
+                        break;
+                    case 'E':
+                    case 'e':
+                        this.grades.Add(20);
+                        break;
+                    default:
+                        throw new Exception("invalid charcter");
+                }
+            }
         }
 
         public Statistics GetStatistics()
         {
-            return new Statistics();
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            foreach (var grade in this.grades)
+            {
+                statistics.Max = Math.Max(statistics.Max, grade);
+                statistics.Min = Math.Min(statistics.Min, grade);
+                statistics.Average += grade;
+            }
+
+            statistics.Average /= this.grades.Count;
+
+            switch (statistics.Average)
+            {
+                case var a when a >= 80:
+                    statistics.AverageLetter = 'A';
+                    break;
+                case var a when a >= 60:
+                    statistics.AverageLetter = 'B';
+                    break;
+                case var a when a >= 40:
+                    statistics.AverageLetter = 'C';
+                    break;
+                case var a when a >= 20:
+                    statistics.AverageLetter = 'D';
+                    break;
+                default:
+                    statistics.AverageLetter = 'E';
+                    break;
+            }
+
+            return statistics;
         }
     }
 }
